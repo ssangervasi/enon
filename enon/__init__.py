3d2f8f9e574d2fafd84fcd6d521d368440a54978
Sebastian Sangervasi <s.ebastian.sangervasi@gmail.com>
2016-09-13 11:14:12 -0700

    Some example emptiness.

--- enon/__init__.py
+++ enon/__init__.py
@@ -1,1 +1,24 @@
 
+class Enon(object):
+  def __str__(self):
+    return ''
+  
+  def __unicode__(self):
+    return u''
+  
+  def __len__(self):
+    return 0
+    
+  def __add__(self, *args, **kwargs):
+    pass
+  
+  def __append__(self, *args, **kwargs):
+    pass
+  
+  def __getitem__(self, *args, **kwargs):
+    return
+  
+  def __setitem__(self, *args, **kwargs):
+    pass
+  
+  
