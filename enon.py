bc812b7ae9c41e312f22b7f8f64a0fbd0f7d2538
ssangervasi <sebastian.sangervasi@gmail.com>
2016-10-03 10:55:30 -0700

    Fixed comparisons, added callable and get/setattr

--- enon.py
+++ enon.py
@@ -22,6 +22,46 @@ class Enon(BaseException, object):
     def __float__(self):
         return 0.0
 
+    # Ha$h money.
+    __hash__ = __int__
+
+    # Behave like something that handles operators.
+    def _ident(self, other):
+        return other
+
+    __add__ = _ident
+    __radd__ = _ident
+    __sub__ = _ident
+    __rsub__ = _ident
+    __mul__ = _ident
+    __rmul__ = _ident
+    __div__ = _ident
+    __rdiv__ = _ident
+
+    def __eq__(self, other):
+        return isinstance(other, Enon)
+
+    def _false(self, *args, **kwargs):
+        return False
+
+    __lt__ = _false
+    __le__ = _false
+    __ne__ = _false
+    __gt__ = _false
+    __ge__ = _false
+
+    # Behave like a junk drawer.
+    def __setattr__(self, name, value):
+        pass
+
+    # Behave like a junk drawer.
+    def __getattr__(self, name):
+        return self
+
+    # Behave like a function.
+    def __call__(self, *args, **kwargs):
+        return self
+
     # Behave like an iterable.
     def __iter__(self):
         return self
@@ -64,19 +104,6 @@ class Enon(BaseException, object):
 
     def flush(self):
         pass
-
-    # Behave like something that handles operators.
-    def _ident(self, other):
-        return other
-
-    __add__ = _ident
-    __radd__ = _ident
-    __sub__ = _ident
-    __rsub__ = _ident
-    __mul__ = _ident
-    __rmul__ = _ident
-    __div__ = _ident
-    __rdiv__ = _ident
 
 
 # The global enon. Praise be unto him.
--- test_enon.py
+++ test_enon.py
@@ -2,6 +2,8 @@
 Withstand the test of time.
 """
 
+from itertools import permutations
+
 from enon import Enon, enon
 
 
@@ -40,6 +42,21 @@ def test_setitem():
     enon['test'] = 'test'
 
 
+def test_get_set_attr():
+    assert enon.test is enon
+    enon.test = 'test'
+    assert enon.test is enon
+
+
+def test_call():
+    args = (1, 'two', range(3))
+    kwargs = {'four': set([4])}
+    assert enon(*args, **kwargs) is enon
+    other_enon = Enon()
+    other_enon_result = other_enon(*args, **kwargs)
+    assert (other_enon is other_enon) and (other_enon is not enon)
+
+
 def test_contains():
     assert 'test' not in enon
     assert 1 not in enon
@@ -70,6 +87,23 @@ def test_write():
 
 def test_flush():
     enon.flush()
+
+
+def test_comps():
+    operable = (
+        set(),
+        -1, 0, 1,
+        -1.5, 0.0, 1.5,
+        '', 'test',
+        list(), tuple(),
+    )
+    for val in operable:
+        for left, right in permutations([val, enon], 2):
+            assert not (left == right)
+            assert not (left < right)
+            assert not (left <= right)
+            assert not (left > right)
+            assert not (left >= right)
 
 
 def test_ops():
